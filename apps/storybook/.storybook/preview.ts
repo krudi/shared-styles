99aa5f8492a9949080a3f830e22e928364c7f599
Patryk Kudlik <patryk.kudlik@gmail.com>
2026-02-06 15:38:11 +0100

    storybook: refine configuration and tooling setup

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,10 +1,24 @@
 import '@krudi/styles/ui';
+import './preview.css';
 
+import { withThemeByClassName } from '@storybook/addon-themes';
 import type { Preview } from '@storybook/html';
 import { MINIMAL_VIEWPORTS } from 'storybook/viewport';
 
 const preview: Preview = {
+    decorators: [
+        withThemeByClassName({
+            themes: {
+                light: '',
+                dark: 'dark',
+            },
+            defaultTheme: 'light',
+        }),
+    ],
     parameters: {
+        backgrounds: {
+            disabled: true,
+        },
         controls: {
             expanded: true,
             matchers: {
@@ -15,9 +29,12 @@ const preview: Preview = {
         layout: 'fullscreen',
         docs: {
             codePanel: true,
+            canvas: {
+                className: `!bg-background`,
+            },
         },
         a11y: {
-            test: 'warn',
+            test: 'error',
         },
         viewport: {
             options: MINIMAL_VIEWPORTS,
