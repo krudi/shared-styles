e922316cdd4aa6dcf8504e7e3e944ac7a73c06d6
Patryk Kudlik <patryk.kudlik@gmail.com>
2026-04-06 14:07:55 +0200

    chore: migrate ESLint/Prettier/Vite configs to TypeScript

---
+++ addons/storybook-design-tokens/prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Options } from 'prettier';
+
+const config: Options = {
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+};
+
+export default config;
