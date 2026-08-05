fc7cfdaea248aff3751e5e627a65138e52505946
Patryk Kudlik <patryk.kudlik@gmail.com>
2026-08-05 16:59:51 +0200

    build(deps): update dependencies and adopt oxlint + oxfmt

--- packages/vue/eslint.config.ts
+++
@@ -1,4 +0,0 @@
-import base from '@krudi/eslint-config/base';
-import prettier from '@krudi/eslint-config/prettier';
-
-export default [...base, ...prettier];
--- packages/vue/prettier.config.ts
+++
@@ -1,8 +0,0 @@
-import base from '@krudi/prettier-config/base';
-import type { Options } from 'prettier';
-
-const config: Options = {
-    ...base,
-};
-
-export default config;
